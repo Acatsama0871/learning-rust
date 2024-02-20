13a36a79d731a69d6f597badafe3fa516ca0e929
HaoHang Li <hli113@stevens.edu>
2024-02-20 21:59:51 +0000

    add update

---
+++ srcs/07_crates_modules/restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+mod font_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+    
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
