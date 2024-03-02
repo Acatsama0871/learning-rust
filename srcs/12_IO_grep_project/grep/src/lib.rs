4301d5085040d8048d2d99b2642f549cf3a1e580
HaoHang Li <hli113@stevens.edu>
2024-03-02 23:58:00 +0000

    finish chpt 12

--- srcs/12_IO_grep_project/grep/src/lib.rs
+++ srcs/12_IO_grep_project/grep/src/lib.rs
@@ -1,30 +1,96 @@
 use std::fs;
+use std::env;
 use std::error::Error;
 
-struct Config {
-    query: String,
-    file_path: String
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
             return Err("The arguments' length should be 3.");
         }
         
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
         
-        Ok(Config {query, file_path})
+        Ok(Config {query, file_path, ignore_case})
     }
 }
 
-pub fn run(args: &[String]) -> Result<(), Box<dyn Error>>{
-    // load config
-    let config = Config::build(&args)?;
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>>{
     // get the file
     let content = fs::read_to_string(&config.file_path)?;
-    println!("With text:\n{}", content);
+    // search
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+    
+    for line in results {
+        println!("{}", line);
+    }
     
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+    
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_case_sensitive() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+safe, fast, productive.
+Pick three.
+Duck Tape";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    
+    #[test]
+    fn test_case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- srcs/12_IO_grep_project/grep/src/main.rs
+++ srcs/12_IO_grep_project/grep/src/main.rs
@@ -1,13 +1,20 @@
 use std::env;
 use std::process;
-use minigrep::run;
+use minigrep::{Config, run};
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    if let Err(e) = run(&args) {
-        println!("Application error: {e}");
+    let config = Config::build(&args).unwrap_or_else(|err| 
+    {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    }
+);
+    
+    if let Err(e) = run(&config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
