eb91ec1c3a06de80126f3091aa6e99c5753f7080
HaoHang Li <hli113@stevens.edu>
2024-03-14 00:36:34 +0000

    fixed the echo issue

--- command-line-rust/echor/src/main.rs
+++ command-line-rust/echor/src/main.rs
@@ -6,18 +6,18 @@ fn main() {
     .author("Haohang Li <hli113.stevens.edu>")
     .about("rust echo")
     .arg(
-        Arg::new("config")
-        .value_name("TEXT")
-        .help("Text input")
-        .required(true)
-        .index(1)
-    ).arg(
         Arg::new("omit_newline")
         .short('n')
         .help("Do not print newline")
         .action(ArgAction::SetTrue)
     )
-    .get_matches();
+    .arg(
+        Arg::new("config")
+        .value_name("TEXT")
+        .help("Text input")
+        .required(true)
+        .num_args(1..)
+    ).get_matches();
 
     println!("{:#?}", matches);
 }
